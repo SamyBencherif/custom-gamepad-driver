18bb47eaac63d8ec4a15e99b085297cb37d38faa
Samie Bee <sbeefm@gmail.com>
2023-08-02 19:41:28 -0400

    starting point for libusb rewrite

--- main.py
+++ main.py
@@ -1,3 +1,26 @@
-
-import pyautogui
+import usb
 import time
+
+"""
+Thanks to https://www.partsnotincluded.com/understanding-the-xbox-360-wired-controllers-usb-data/
+"""
+
+gamepad = usb.core.find(idVendor=0x045e, idProduct=0x028e)
+
+if gamepad is None:
+  print("Waiting for gamepad...")
+  while gamepad is None:
+    gamepad = usb.core.find(idVendor=0x045e, idProduct=0x028e)
+    time.sleep(.1)
+
+
+gamepad.set_configuration()
+
+while True:
+  B = gamepad.read(0x81, 20)
+
+  # ignore messages I don't understand
+  if B[0] != 0 or B[1] != 20:
+    continue
+
+  print("[A] :", ((B[3]>>4)&1))
